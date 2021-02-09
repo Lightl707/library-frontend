68b46faf9ed31d27182034a62a71a59a944bd68d
Lightl707 <Lightl707@mail.ru>
2021-02-09 13:12:18 +0600

    one more fix

--- pages/crud-book.js
+++ pages/crud-book.js
@@ -69,10 +69,10 @@ const CrudBook = () => {
                 <p>{item.id}</p>
               </td>
               <td>
-        {item.original_title.text === null ? <p>{item.original_title.text}</p> : <p>Ошибка</p> }
+        {item.original_title.text != null ? <p>{item.original_title.text}</p> : <p>Ошибка</p> }
               </td>
               <td>
-                <p>{item.author.nickname}</p>
+                 {item.author.nickname != null ? <p>{item.author.nickname}</p> : <p>Автор был удалён</p> }
               </td>
               <td>
                 <p>{item.description}</p>
